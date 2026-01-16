4cc095fd226a13dbc0471054fe6c1e5a0837b0c6
Iván Vega García <pkp23714@educastur.es>
2026-01-16 12:46:12 +0100

    Cafetera src hecha

--- src/src/Cafetera.java
+++ src/src/Cafetera.java
@@ -1,6 +1,70 @@
 package src;
 
 public class Cafetera {
-	private double capacidadMaxima;
-	private double cantidadActual;
+	private int capacidadMaxima;
+	private int cantidadActual;
+
+	//CONSTRUCTORES
+	public Cafetera() {
+		this(1000, 0);
+	}
+
+	public Cafetera(int capacidadMaxima) {
+		this(capacidadMaxima, capacidadMaxima);
+	}
+
+	public Cafetera(int capacidadMaxima, int cantidadActual) {
+		this.capacidadMaxima = capacidadMaxima;
+		this.cantidadActual = 
+			cantidadActual > capacidadMaxima 
+				? capacidadMaxima 
+				: cantidadActual;
+	}
+
+	//GETTERS Y SETTERS
+	public double getCantidadActual() {
+		return this.cantidadActual;
+	}
+	public void setCantidadActual(int newCantidad) {
+		this.cantidadActual = newCantidad;
+	}
+	public double getCapacidadMaxima() {
+		return this.capacidadMaxima;
+	}
+	public void setCapacidadMaxima(int newCapacidad) {
+		this.capacidadMaxima = newCapacidad;
+	}
+	
+	//METODOS
+	public void llenarCafetera() {
+		 this.cantidadActual = this.capacidadMaxima;
+	}
+	
+	public int servirTaza(int cc) {
+		int servido = 0;
+		if(cantidadActual== 0) {
+			System.out.println("Lo sentimos, no queda café.");
+			servido = 0;
+		}else {
+			if(cantidadActual <= cc) {
+				System.out.println("Se ha servido todo lo que quedaba ("+cantidadActual+" c.c)");
+				servido = cantidadActual;
+				cantidadActual = 0;
+			}else {
+				servido = cc;
+				cantidadActual -= cc;
+				System.out.println("Todavía quedan "+cantidadActual + " c.c)");
+			}
+		}
+		return servido;
+	}
+	
+	public void vaciarCafetera() {
+		this.cantidadActual = 0;
+	}
+	
+	public void agregarCafe(int cc) {
+		cantidadActual += cc;
+	}
+	
 }
--- test/pruebas/CafeteraTest.java
+++ test/pruebas/CafeteraTest.java
@@ -1,14 +1,31 @@
 package pruebas;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import src.Cafetera;
 
 class CafeteraTest {
 
+
+	private Cafetera cafetera;
+	
+	@BeforeEach
+	void crearCafetera() {
+		cafetera = new Cafetera();
+	}
+
+	/**
+	 * GIVEN: Una cuenta bancaria nueva y vacia
+	 * WHEN: Se intenta ingresar una cantidad positiva
+	 * THEN: El saldo se incrementa adecuadamente
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testIngresarCuentaVacia() {
+		
 	}
 
 }
